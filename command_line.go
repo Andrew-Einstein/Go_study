9ebd07b3784b532fb6861e608f818527d13d69cd
kiki <shenlv0622@163.com>
2019-07-03 16:42:45 +0800

    更新学习代码

--- command_line.go
+++ command_line.go
@@ -6,12 +6,10 @@ import (
 	"strings"
 )
 
-// 命令行参数
 func main() {
-	who := "World!"
+	who := "KIKI"
 	if len(os.Args) > 1 {
-		// os.Args[0] is CommandLine.exe or CommandLine
 		who = strings.Join(os.Args[1:], " ")
 	}
-	fmt.Println("hello", who)
+	fmt.Println("你好", who)
 }
